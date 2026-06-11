bc84ff69e84f588491c3deda2c418b348e0a3ff0
[ progorker ] Software Development as a Platform <zinospetrel@sdf.org>
2026-06-11 13:26:19 +0700

    Add files via upload

---
+++ phpTestor/testor-cfg.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Copyright (c) 2026 Dinh Thoai Tran <zinospetrel@sdf.org>
+ * All rights reserved.
+ *
+ * + Source URL: https://github.com/progorker/phptestor/
+ *
+ * + License: GPL-2.0
+ */
+
+namespace phptestor;
+
+global $g_config;
+
+$g_config = array(
+ 'mytestor.host' => '192.168.230.203',
+ 'mytestor.port' => '3306',
+ 'mytestor.username' => 'mytestor',
+ 'mytestor.password' => 'kunqhtsadzmopeh',
+ 'mytestor.database' => 'mytestor'
+);
+?>
